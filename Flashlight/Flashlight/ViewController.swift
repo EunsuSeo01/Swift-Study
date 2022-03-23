3207906154ef8f8937f0f326ab0de9cc3a9fe8d6
EunsuSeo01 <eunsu0613@naver.com>
2022-03-23 16:36:00 +0900

    dev : 아이폰 플래시도 같이 켜지는 기능 추가

--- Flashlight/Flashlight/ViewController.swift
+++ Flashlight/Flashlight/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import AVFoundation     // 사운드 파일 재생을 위해.
+import AVFoundation     // 사운드 파일 재생, 플래시를 켜기 위해.
 
 class ViewController: UIViewController {
 
@@ -36,12 +36,36 @@ class ViewController: UIViewController {
         }
     }
     
+    // 핸드폰의 플래시를 켜주는 함수.
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
     // 버튼을 눌렀을 때 일어날 동작(액션)을 설정하는 것. -> IBAction의 Touch Up Inside
     // 버튼을 눌렀을 때 버튼의 이미지와 배경 이미지를 변경하고자 함.
     @IBAction func switchTapped(_ sender: Any) {
         isOn = !isOn    // 버튼을 누를 때마다 on/off 상태가 바뀌니까!
         
         soundPlayer?.play();    // 버튼 누를 때마다 사운드 재생.
+        toggleTorch(on: isOn)   // isOn == true일 때 핸드폰의 플래시를 켠다.
         
         // 방법 3 : 삼항 연산자를 써서 간단하게
         switchButton.setImage(UIImage(named: isOn ? "onSwitch" : "offSwitch"), for: .normal)
