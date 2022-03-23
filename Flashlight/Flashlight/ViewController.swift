cd887a88c6bd67535a5ec4ba9bfa77559ea594cc
EunsuSeo01 <eunsu0613@naver.com>
2022-03-23 16:22:19 +0900

    dev : 버튼 사운드 재생 로직 구현

--- Flashlight/Flashlight/ViewController.swift
+++ Flashlight/Flashlight/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation     // 사운드 파일 재생을 위해.
 
 class ViewController: UIViewController {
 
@@ -14,14 +15,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var flashImageView: UIImageView!
     var isOn = false
     
+    var soundPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        prepareSound();
+    }
+    
+    // 사운드 play를 준비하는 함수. -> 사운드 파일의 경로를 가져와서 플레이어를 만든다.
+    func prepareSound() {
+        let path = Bundle.main.path(forResource: "switch.wav", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.prepareToPlay()    // play 전에 미리 메모리에 로드해두고 준비하는 것.
+        } catch {
+            // couldn't load file :(
+        }
     }
     
     // 버튼을 눌렀을 때 일어날 동작(액션)을 설정하는 것. -> IBAction의 Touch Up Inside
     // 버튼을 눌렀을 때 버튼의 이미지와 배경 이미지를 변경하고자 함.
     @IBAction func switchTapped(_ sender: Any) {
         isOn = !isOn    // 버튼을 누를 때마다 on/off 상태가 바뀌니까!
+        
+        soundPlayer?.play();    // 버튼 누를 때마다 사운드 재생.
+        
+        // 방법 3 : 삼항 연산자를 써서 간단하게
+        switchButton.setImage(UIImage(named: isOn ? "onSwitch" : "offSwitch"), for: .normal)
+        flashImageView.image = UIImage(named: isOn ? "onBG" : "offBG")
+        
         
         // 방법 1 : 오리지널 방법
 //        if isOn {
@@ -46,9 +71,6 @@ class ViewController: UIViewController {
 //        switchButton.setImage(UIImage(named: "onSwitch"), for: .normal)
 //        flashImageView.image = #imageLiteral(resourceName: "offBG")
         
-        // 방법 3 : 삼항 연산자를 써서 간단하게
-        switchButton.setImage(UIImage(named: isOn ? "onSwitch" : "offSwitch"), for: .normal)
-        flashImageView.image = UIImage(named: isOn ? "onBG" : "offBG")
     }
 
 }
