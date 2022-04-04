cac519ed928a2196eccb94f1294f8be37dd72f24
EunsuSeo01 <eunsu0613@naver.com>
2022-04-04 23:10:54 +0900

    dev : title, tableView 추가 & navigation controller 추가

--- OurToDoList/OurToDoList/ViewController.swift
+++ OurToDoList/OurToDoList/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    // tableView 생성.
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 1. title 설정
+        title = "To Do List"
+        
+        // 2. tableView 추가
+        view.addSubview(table)
+        table.dataSource = self
+    }
+    
+    // table에 frame을 줘야 한다.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
     }
 
-
+    // dataSource 설정
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
 }
 
