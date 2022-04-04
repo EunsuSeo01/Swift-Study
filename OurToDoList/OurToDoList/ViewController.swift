c35b7722ca8b43864db52491d06f1b2a21302c76
EunsuSeo01 <eunsu0613@naver.com>
2022-04-05 00:57:40 +0900

    dev : alert 띄우기 & Done 버튼 로직 추가

--- OurToDoList/OurToDoList/ViewController.swift
+++ OurToDoList/OurToDoList/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController, UITableViewDataSource {
         return table
     }()
     
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,7 +28,41 @@ class ViewController: UIViewController, UITableViewDataSource {
         // 2. tableView 추가
         view.addSubview(table)
         table.dataSource = self
+        
+        // 3. 오른쪽 위에 + 버튼 추가 (+ 버튼을 눌렀을 때 실행될 action 설정도 가능!)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(didTapAdd))
     }
+    
+    // + 버튼을 눌렀을 때 실행될 action 메소드 정의. (alert 띄우기)
+    @objc
+    private func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter new to do list item", preferredStyle: .alert)
+        
+        alert.addTextField { field in
+            field.placeholder = "Enter item..."
+        }
+        
+        // Cacel 버튼 생성.
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        // Done 버튼 생성 & 실행할 동작 정의.
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self] (_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    
+                    // Enter new to do list item
+                    DispatchQueue.main.async {
+                        self?.items.append(text)
+                        self?.table.reloadData()
+                    }
+                }
+            }
+        }))
+        
+        present(alert, animated: true)
+    }
+    
     
     // table에 frame을 줘야 한다.
     override func viewDidLayoutSubviews() {
@@ -34,13 +70,17 @@ class ViewController: UIViewController, UITableViewDataSource {
         table.frame = view.bounds
     }
 
-    // dataSource 설정
+    // dataSource 설정.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = items[indexPath.row]
+        cell.contentConfiguration = content
         
         return cell
     }
