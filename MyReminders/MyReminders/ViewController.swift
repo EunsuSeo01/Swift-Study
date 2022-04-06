491b93eca3d68886ee757f3da85ac6340844ded5
EunsuSeo01 <eunsu0613@naver.com>
2022-04-06 23:35:17 +0900

    dev : tableView 기본 설정

--- MyReminders/MyReminders/ViewController.swift
+++ MyReminders/MyReminders/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var table: UITableView!
+    var models = [MyReminder]()     // 구조체는 생성자가 자동 생성.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.delegate = self
+        table.dataSource = self
     }
 
 
 }
 
+
+// tableView를 쓰기 위해 delegate와 dataSource 설정.
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        
+        return cell
+    }
+}
+
+// reminder 객체를 담을 구조체 생성.
+struct MyReminder {
+    var title: String
+    var date: Date
+    var identifier: String
+}
