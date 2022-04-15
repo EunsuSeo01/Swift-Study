e4c3279a556d047f493db8e975a8f31c21dc58de
EunsuSeo01 <eunsu0613@naver.com>
2022-04-16 00:24:30 +0900

    Components - initial commit (No storyboard setting)

---
+++ Components/Components/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Components
+//
+//  Created by 서은수 on 2022/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var mainButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "main"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+
+        view.addSubview(mainButton)
+        mainButton.addSubview(mainLabel)
+        
+        mainButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        mainButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        mainButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        mainLabel.centerXAnchor.constraint(equalTo: mainButton.centerXAnchor).isActive = true
+        mainLabel.centerYAnchor.constraint(equalTo: mainButton.centerYAnchor).isActive = true
+    }
+
+
+}
+
