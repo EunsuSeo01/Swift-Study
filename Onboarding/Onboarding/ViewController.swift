2ed93d2f948acbce6a18f5be62e039432d8ca47f
EunsuSeo01 <eunsu0613@naver.com>
2022-04-18 20:39:27 +0900

    dev : 온보딩 화면을 띄워야 하는 경우인지 아닌지에 대한 로직 구현, 온보딩 화면을 띄울 WelcomeVC 생성

--- Onboarding/Onboarding/ViewController.swift
+++ Onboarding/Onboarding/ViewController.swift
@@ -11,9 +11,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // 새 유저라면 onboarding을 띄워준다.
+        if Core.shared.isNewUser() {
+            // Show onboarding.
+            let vc = storyboard?.instantiateViewController(withIdentifier: "welcome") as! WelcomeViewController
+            
+            // 전체 화면으로 present 되도록 설정.
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true)
+        }
+    }
 }
 
+// Underlying logic (Show different view based on NewUser or ExistingUser)
+class Core {
+    
+    static let shared = Core()
+    
+    // 새 유저인지 확인. (새 유저라면 onboarding flow를 보여줘야 함)
+    func isNewUser() -> Bool {
+        return !UserDefaults.standard.bool(forKey: "isNewUser")
+    }
+    
+    // 이제는 새 유저가 아닌 걸로 세팅. => onboarding flow를 이미 본 사람으로 세팅.
+    func setIsNotNewUser() {
+        UserDefaults.standard.set(true, forKey: "isNewUser")
+    }
+}
---
+++ Onboarding/Onboarding/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  Onboarding
+//
+//  Created by 서은수 on 2022/04/12.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var holderView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red     // for test
+    }
+    
+    
+    
+    
+}
