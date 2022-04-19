481dbe8608e885c5909fd0eb0a4e0416dd4c6fe2
EunsuSeo01 <eunsu0613@naver.com>
2022-04-20 01:08:53 +0900

    fix : 시간 나타내는 label 구성 변경, play 버튼 에러 수정
    gif와 더 비슷하게 만들기 위해서 하나의 label로 구성했던 시간(0.0)을 더 쪼개서 3개의 label로 구성함.
    play 버튼을 여러번 클릭했을 때 Timer가 여러 개 만들어져서 초가 매우 빠르게 변하는 에러가 있었는데 이를 수정함.

--- SimpleStopWatch/SimpleStopWatch/ViewController.swift
+++ SimpleStopWatch/SimpleStopWatch/ViewController.swift
@@ -11,14 +11,36 @@ class ViewController: UIViewController {
 
     // MARK: - Subviews
     
-    // label 생성.
-    lazy var secondsLabel: UILabel = {
+    // 초(seconds) 부분을 나타내는 label 생성.
+    lazy var seconds: UILabel = {
         let label = UILabel()
-        label.text = "0.0"
         label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 100, weight: .ultraLight)
-        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 90, weight: .ultraLight)
         label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.text = "0"
+        return label
+    }()
+    
+    // 소수점을 나타내는 label 생성.
+    lazy var decimalPoint: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 90, weight: .ultraLight)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.text = "."
+        return label
+    }()
+    
+    // 소수점 이하 첫번째 자리를 나타내는 label 생성.
+    lazy var firstDigit: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 90, weight: .ultraLight)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.text = "0"
         return label
     }()
     
@@ -74,6 +96,9 @@ class ViewController: UIViewController {
     // stop watch가 작동 중인지 아닌지.
     lazy var isRunning = false
     
+    // Timer의 중복 생성을 막기 위해 (하나의 Timer만 돌아가도록 하기 위해) 만든 구분 변수.
+    lazy var initialTimerNum = 0
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -95,7 +120,9 @@ class ViewController: UIViewController {
     // 서브뷰 설정.
     func setSubviews() {
         // view의 서브뷰로 추가.
-        view.addSubview(secondsLabel)
+        view.addSubview(seconds)
+        view.addSubview(decimalPoint)
+        view.addSubview(firstDigit)
         view.addSubview(playView)
         view.addSubview(pauseView)
         view.addSubview(resetButton)
@@ -104,9 +131,18 @@ class ViewController: UIViewController {
         playView.addSubview(playButton)
         pauseView.addSubview(pauseButton)
         
-        // label 레이아웃 설정.
-        secondsLabel.centerYAnchor.constraint(equalTo: view.topAnchor, constant: UIScreen.main.bounds.size.height/5).isActive = true
-        secondsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        // 시간에 대한 label 레이아웃 설정.
+        // 소수점의 레이아웃 설정.
+        decimalPoint.centerYAnchor.constraint(equalTo: view.topAnchor, constant: UIScreen.main.bounds.size.height/5).isActive = true
+        decimalPoint.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        
+        // 초(seconds) label의 레이아웃 설정.
+        seconds.centerYAnchor.constraint(equalTo: view.topAnchor, constant: UIScreen.main.bounds.size.height/5).isActive = true
+        seconds.rightAnchor.constraint(equalTo: decimalPoint.leftAnchor, constant: -5).isActive = true
+        
+        // 소수점 아래 첫번째 자리 label의 레이아웃 설정.
+        firstDigit.centerYAnchor.constraint(equalTo: view.topAnchor, constant: UIScreen.main.bounds.size.height/5).isActive = true
+        firstDigit.leftAnchor.constraint(equalTo: decimalPoint.rightAnchor, constant: 5).isActive = true
         
         // playView 레이아웃 설정.
         NSLayoutConstraint.activate([playView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
@@ -135,17 +171,27 @@ class ViewController: UIViewController {
     
     // play 버튼 누르면 stop watch 시작!
     @objc func startStopWatch() {
-        isRunning = true
+        isRunning = true    // 작동 중.
+        initialTimerNum += 1    // Timer는 1개.
         
-        // 0.1초 시간 간격. -> 0.1초가 10번 지나면 1초.
-        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (Timer) in
-            // stop watch가 작동 중이면,
-            // 0.1초마다 label의 텍스트도 0.1을 더한 숫자가 보이도록 설정한다.
-            if self.isRunning {
-                self.secondsLabel.text = String(format: "%.1f", Double(self.secondsLabel.text!)! + 0.1)
-                print ("\(self.secondsLabel.text!) seconds")
-            } else {
-                Timer.invalidate()
+        // Timer의 중복 생성을 막기 위해 작동 중에 Timer를 한 번만 만든다.
+        if self.initialTimerNum == 1 {
+            // 0.1초 시간 간격. -> 0.1초가 10번 지나면 1초.
+            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (Timer) in
+                // stop watch가 작동 중이면,
+                // 0.1초마다 label의 텍스트도 0.1을 더한 숫자가 보이도록 설정한다.
+                if self.isRunning {
+                    // 소수점 아래 첫번째 자리 숫자 1 증가시킴.
+                    self.firstDigit.text = String(Int(self.firstDigit.text!)! + 1)
+                    
+                    // 소수점 아래 첫번째 자리가 10이 되면 1초가 됨. -> 1초 증가.
+                    if self.firstDigit.text == "10" {
+                        self.firstDigit.text = "0"      // 소수점 아래 첫번째 자리는 다시 0으로 초기화.
+                        self.seconds.text = String(Int(self.seconds.text!)! + 1)    // 1초 증가.
+                    }
+                } else {
+                    Timer.invalidate()
+                }
             }
         }
     }
@@ -153,11 +199,14 @@ class ViewController: UIViewController {
     // pause 버튼 누르면 stop watch 멈춤. (작동 중 아님)
     @objc func pauseStopWatch() {
         isRunning = false
+        initialTimerNum = 0
     }
     
-    // seconds를 reset 한다.
+    // seconds와 firstDigit를 reset 한다.
     @objc func resetSeconds() {
-        secondsLabel.text = "0.0"
+        seconds.text = "0"
+        firstDigit.text = "0"
+        initialTimerNum = 0
     }
 }
 
