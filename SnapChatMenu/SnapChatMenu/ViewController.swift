6ff4b7c3f5ae66ba5478ea40c11473156d5e3f91
EunsuSeo01 <eunsu0613@naver.com>
2022-04-27 19:45:46 +0900

    SnapChatMenu - initial commit (No storyboard)

---
+++ SnapChatMenu/SnapChatMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapChatMenu
+//
+//  Created by 서은수 on 2022/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
