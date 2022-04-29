3a3dc9164bd756e27071e431c09a85a8ce92cb82
EunsuSeo01 <eunsu0613@naver.com>
2022-04-29 23:50:33 +0900

    MemoService - initial commit

---
+++ MemoService/MemoService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoService
+//
+//  Created by 서은수 on 2022/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
