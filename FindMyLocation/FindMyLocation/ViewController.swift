67b26b22f90d89f8b88826a923912425e0c9c205
EunsuSeo01 <eunsu0613@naver.com>
2022-05-09 23:16:07 +0900

    dev : 배경 구현

--- FindMyLocation/FindMyLocation/ViewController.swift
+++ FindMyLocation/FindMyLocation/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Subviews
+    
+    @IBOutlet weak var mainBackgroundImageView: UIImageView!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        makeBackgroundBlur()
     }
 
-
+    
+    /// background에 blur 효과 주기.
+    /// - UIVisualEffectView를 background 크기에 맞게 만들어서 addSubview로 올려주는 방법 사용!
+    func makeBackgroundBlur() {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurredView = UIVisualEffectView(effect: blurEffect)
+        blurredView.frame = self.view.bounds
+        mainBackgroundImageView.addSubview(blurredView)
+    }
 }
 
