6a2e3d96b11d55659edb823a362c791700b6114a
EunsuSeo01 <eunsu0613@naver.com>
2022-05-16 00:07:19 +0900

    dev : 그라데이션 컬러 설정

--- RandomGradientColorMusic/RandomGradientColorMusic/ViewController.swift
+++ RandomGradientColorMusic/RandomGradientColorMusic/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var musicPlayImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.setGradient(color0: .white, color1: .red, color2: .green, color3: .blue)
+        view.addSubview(musicPlayImageView)
     }
 
 
 }
 
+extension UIView{
+    
+    func setGradient(color0: UIColor, color1: UIColor, color2: UIColor, color3: UIColor){
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [color0.cgColor, color1.cgColor, color0.cgColor, color2.cgColor, color0.cgColor, color3.cgColor, color0.cgColor,]
+        gradient.locations = [0.15, 0.2, 0.4, 0.45, 0.65, 0.7]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.frame = bounds
+        layer.addSublayer(gradient)
+    }
+}
