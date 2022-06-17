b8bda53d19b8ad6f6c0563f6b1fa9dfb07f5f0e7
EunsuSeo01 <eunsu0613@naver.com>
2022-06-17 23:59:00 +0900

    Xylophone - feat : Xylophone 버튼에 소리 연결

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    // 모든 버튼과 이 함수를 연결.
+    @IBAction func keyPressed(_ sender: UIButton) {
+        // UIButton의 title을 soundName으로 전달.
+        playSound(soundName: sender.currentTitle!)
+    }
     
-
+    func playSound(soundName: String) {
+        // soundName에 맞는 wav 파일을 찾아 url을 생성.
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        
+        // url로 player를 만들고 재생.
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
 }
 
