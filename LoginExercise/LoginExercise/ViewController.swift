1dc6e089279ccb23aa65bd447e24fc13c2b475f6
EunsuSeo01 <eunsu0613@naver.com>
2022-07-01 03:50:25 +0900

    LoginExercise - Feat: No storyboard setting

---
+++ LoginExercise/LoginExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginExercise
+//
+//  Created by 서은수 on 2022/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
