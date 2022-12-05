83052c7f7bc5f631f1b621e9daf18c032997e6ec
EunSuSeo01 <eunsu0613@naver.com>
2022-12-05 16:28:45 +0900

    StarscreamTest - initial commit
    Starscream 라이브러리를 Geeksasaeng 프로젝트에 직접 적용하기 전에 써보고 테스트 해보려고 만든 프로젝트

---
+++ StarscreamTest/StarscreamTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarscreamTest
+//
+//  Created by 서은수 on 2022/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
